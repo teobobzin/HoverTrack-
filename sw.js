53a2e5854d68cbf57fd90e8f527a64302623709d
teobobzin <teobobzin@gmail.com>
2026-02-17 20:16:31 -0800

    feat: Update PWA manifest and service worker
    Adjusts PWA manifest for standalone display and correct start URL. Enhances service worker to register a route for offline caching of all routes using StaleWhileRevalidate strategy.

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-// This is the "Offline page" service worker
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+const CACHE = "pwabuilder-offline-page";
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-offline";
 const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
@@ -21,6 +23,13 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
@@ -34,6 +43,7 @@ self.addEventListener('fetch', (event) => {
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
+
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
